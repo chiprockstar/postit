7f9e96a79f8a40f4a4490a0ebff55ec24ab1736f
Chip Self <chipself9@gmail.com>
2015-03-27 00:39:23 -0400

    crud and error messages

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,19 +1,27 @@
 class PostsController < ApplicationController
 
+  before_action :set_post, only: [:show, :edit, :update]
+
   def index
     @posts = Post.all
   end
 
   def show
-    @post = Post.find(params[:id])
+
   end
 
   def new
-
+    @post = Post.new
   end
 
   def create
-
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:notice] = "Your post was created."
+      redirect_to posts_path
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -21,7 +29,21 @@ class PostsController < ApplicationController
   end
 
   def update
-
+    if @post.update(post_params)
+      flash[:notice] = "The Post was updated."
+      redirect_to posts_path
+    else
+      render :edit
+    end
   end
 
+  private
+
+  def post_params
+    params.require(:post).permit!
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
 end
