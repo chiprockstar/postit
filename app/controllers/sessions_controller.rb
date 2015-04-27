aea561cc835f2816ff9b7a89abc129f09012303b
Chip Self <chipself9@gmail.com>
2015-04-27 19:57:09 +0000

    edit/show user profile

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -20,7 +20,7 @@ class SessionsController < ApplicationController
   def destroy
     session[:user_id] = nil
     flash['notice'] = "You've logged out!"
-    redirect_to login_path
+    redirect_to root_path
     
   end
   
