cad8ee37486193d27577bbfff98637d39dc28b66
Chip Self <chipself9@gmail.com>
2015-04-28 23:42:15 +0000

    spelling error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash['notice'] = "Your are registered."
+      flash['notice'] = "You are registered."
       session[:user_id] = @user.id
       redirect_to root_path
     else
